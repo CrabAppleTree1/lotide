3506c23903919452fc332b08832c2c4074cd5884
haiming <hesmaeilz01@gmail.com>
2021-09-14 18:39:38 -0400

    asserting equaluity of arrays, all test cases pass

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function (a1, a2) {
+  condition = true
+  for (let i = 0; i < a1.length; i++) {
+
+    if (a1[i] !== a2[i]) {
+      condition = false
+      break;
+    }
+  }
+  return condition
+}
+
+const assertArraysEqual = function (funct, cond) {
+  funct === cond ? console.log('✅ Assertion Passed') : console.log('🛑    Assertion Failed:');
+}
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
