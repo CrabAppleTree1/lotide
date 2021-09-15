efcdceb752dc6b4a351983304200079f949ecf59
haiming <hesmaeilz01@gmail.com>
2021-09-15 14:08:34 -0400

    passes all assertEquals

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+ 
+  (actual === expected) ? console.log(`✅ Assertion Passed:${actual} === ${expected}`) : console.log(`🛑   Assertion Failed: ${actual} !== ${expected}`);
+  
+};
+
+const countOnly = function(allItems, itemsToCount) {
+
+  const targetStats = {};
+  for (const item of allItems){
+    itemsToCount[item] ? 
+      targetStats[item] ?
+      targetStats[item] += 1 : targetStats[item] = 1
+    : true 
+  }
+return targetStats;
+}
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
